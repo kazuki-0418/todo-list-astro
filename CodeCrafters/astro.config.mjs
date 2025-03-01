24f72b28b967f6fbbae81aa1be88136cc89edb3d
kazuki-0418 <kazuki.castle0418@gmail.com>
2025-02-28 19:00:55 -0800

    Updated

--- CodeCrafters/astro.config.mjs
+++ CodeCrafters/astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  output: "server",
+  adapter: vercel(),
+  devToolbar: {
+    enabled: false,
+  },
 });
